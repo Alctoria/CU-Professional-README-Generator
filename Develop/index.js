5945f7077d7b1f294797fce27c7e9df89f58d093
Alctoria <defruits2006@gmail.com>
2024-06-28 22:32:11 +0900

    added questions
    added questions im going to ask in node

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
 
+
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        "type": "input",
+        "message": "What is the name of your project?",
+        "name": "title"
+    },
+    {
+        "type": "input",
+        "message": "Please describe the project.",
+        "name": "description"
+    },
+    {
+        "type": "input",
+        "message": "Please describe the installation instructions for the project.",
+        "name": "installation"
+    },
+    {
+        "type": "input",
+        "message": "Please describe how to use the project.",
+        "name": "usage"
+    },
+    {
+        "type": "list",
+        "message": "What license do you want to use for this project?",
+        "name": "license",
+        "choices": ["No license", "MIT", "ISC", "Apache 2.0", "GNU GPLv2", "GNU GPLv3", "GNU AGPLv3", "GNU LGPLv3", "Mozilla Public 2.0", "Boost Software 1.0", "The Unlicense"]
+    },
+    {
+        "type": "input",
+        "message": "Describe any credit you wish to attribute for the work done in this project.",
+        "name": "credit"
+    },
+    {
+        "type": "input",
+        "message": "Please describe the contribution guidelines for the project.",
+        "name": "contributing"
+    },
+    {
+        "type": "input",
+        "message": "Please describe the test instructions for the project.",
+        "name": "tests"
+    },
+    {
+        "type": "input",
+        "message": "Please enter your github username.",
+        "name": "github"
+    },
+    {
+        "type": "input",
+        "message": "Please enter your email address.",
+        "name": "email"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
